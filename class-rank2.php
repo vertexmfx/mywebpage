054c50b7a8edbb88841183b0eedd42a511453d7b
vertex <vertexmfx@gmail.com>
2020-01-10 11:15:57 +0800

    jj

--- class-rank2.php
+++ class-rank2.php
@@ -4,9 +4,6 @@ include "mysqlfunc.php";
 if(empty($_COOKIE['usrname'])){
     header("Location:login.php");
 }
-if($_COOKIE['usrname']!="易泰河" && $_COOKIE['usrname']!='王正明'){
-    header("Location:class-rank.php");
-}
 $namelist=select_ranks($conn,$_COOKIE['usrname']);
 $namelistJiangke=$namelist[0];
 $namelistChuti=$namelist[1];
